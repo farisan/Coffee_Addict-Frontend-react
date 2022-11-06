827ef4cd221dece2d306901e802fae5d0f3406b5
farisan <muhammad.farisan99@gmail.com>
2022-11-07 01:01:49 +0700

    new install library

--- src/router.js
+++ src/router.js
@@ -10,6 +10,12 @@ import ProductDetail from "./pages/ProductDetail";
 import ForgotPWD from "./pages/ForgotPWD";
 import Payment from "./pages/Payment";
 import History from "./pages/History";
+import Updateproduct from "./pages/admin/UpdateProduct"
+import Updatepromo from "./pages/admin/UpdatePromo"
+import Handlingproduct from "./pages/admin/Handlingproduct"
+
+import PrivateElement from "./components/PrivateElement.js";
+
 
 // import Error from "./pages/Error";
 
@@ -18,12 +24,60 @@ const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/login", element: <Login /> },
     { path: "/signup", element: <Signup /> },
-    { path: "/profile", element: <Profile /> },
     { path: "/product", element: <Product /> },
-    { path: "/productdetail", element: <ProductDetail /> },
-    { path: "/forgotpassword", element: <ForgotPWD /> },
-    { path: "/payment", element: <Payment /> },
-    { path: "/history", element: <History /> },
+    {
+        path: "/profile", element:
+            <PrivateElement allowedRoles={["user"]}>
+                <Profile />
+            </PrivateElement>
+    },
+    {
+        path: "/productdetail/:id", element:
+            <PrivateElement allowedRoles={["user"]}>
+                <ProductDetail />
+            </PrivateElement>
+    },
+    {
+        path: "/forgotpassword", element:
+            <PrivateElement allowedRoles={["user"]}>
+                <ForgotPWD />
+            </PrivateElement>
+    },
+    {
+        path: "/payment", element:
+            <PrivateElement allowedRoles={["user"]}>
+                <Payment />
+            </PrivateElement>
+    },
+    {
+        path: "/history", element:
+            <PrivateElement allowedRoles={["user"]}>
+                <History />
+            </PrivateElement>
+    },
+
+
+
+
+    {
+        path: "/updateproduct", element:
+            <PrivateElement allowedRoles={["admin"]}>
+                <Updateproduct />
+            </PrivateElement>
+    },
+    {
+        path: "/updatepromo", element:
+            <PrivateElement allowedRoles={["admin"]}>
+                <Updatepromo />
+            </PrivateElement>
+    },
+    {
+        path: "/handlingproduct", element:
+            <PrivateElement allowedRoles={["admin"]}>
+                <Handlingproduct />
+            </PrivateElement>
+    },
+
 ]);
 
 export default router;
