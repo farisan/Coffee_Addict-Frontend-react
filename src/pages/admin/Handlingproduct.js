e72ea974805f9ddedbfff4e94442ba4bea252254
farisan <muhammad.farisan99@gmail.com>
2022-11-07 01:05:50 +0700

    no crud

---
+++ src/pages/admin/Handlingproduct.js
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import axios from "axios";
+import withParams from "../../helpers/withRouteParams";
+
+// import css
+import styles from "../../styles/adminCSS/Handlingproduct.module.css"
+
+// import component
+import Navbar from "../../components/Navbar"
+import NavbarAdmin from "../../components/NavbarAdmin"
+import NavbarnotLogin from "../../components/Navbar-notLogin"
+import Footer from "../../components/Footer"
+import CardPromo from "../../components/CardPromo"
+import Cardproduct from "../../components/Card-Product"
+import titlebar from "../../utility/WebDinamis"
+
+
+
+
+class Handlingproduct extends Component {
+
+
+    state = {
+        product: [],
+        favorite: `${process.env.REACT_APP_BACKEND_HOST}coffee/product/?sorting=favorite&page=1&limit=12`,
+        coffee: `${process.env.REACT_APP_BACKEND_HOST}coffee/product/?category=coffee&page=1&limit=12`,
+        non_coffee: `${process.env.REACT_APP_BACKEND_HOST}coffee/product/?category=non_coffee&page=1&limit=12`,
+        food: `${process.env.REACT_APP_BACKEND_HOST}coffee/product/?category=foods&page=1&limit=12`,
+        addons: `${process.env.REACT_APP_BACKEND_HOST}coffee/product/?category=addon&page=1&limit=12`,
+        navLogin: <Navbar />,
+        navAdmin: <NavbarAdmin />,
+        navnotLogin: <NavbarnotLogin />,
+    };
+    componentDidMount() {
+        axios.get(this.state.favorite)
+            .then((res) => {
+                this.setState({ product: res.data.result.data })
+            })
+            .catch((err) => console.log(err));
+    }
+
+    costing = (price) => {
+        return 'IDR ' + parseFloat(price).toFixed().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    }
+
+    Favorite = () => {
+        axios
+            .get(this.state.url)
+            .then((res) => this.setState({ product: res.data.result.data }))
+            .catch((err) => console.log(err));
+
+    };
+    Coffee = () => {
+        axios
+            .get(this.state.coffee)
+            .then((res) => this.setState({ product: res.data.result.data }))
+            .catch((err) => console.log(err));
+    };
+    NonCoffee = () => {
+        axios
+            .get(this.state.non_coffee)
+            .then((res) => this.setState({ product: res.data.result.data }))
+            .catch((err) => console.log(err));
+    };
+    Food = () => {
+        axios
+            .get(this.state.food)
+            .then((res) => this.setState({ product: res.data.result.data }))
+            .catch((err) => console.log(err));
+    };
+    AddOn = () => {
+        axios
+            .get(this.state.addons)
+            .then((res) => this.setState({ product: res.data.result.data }))
+            .catch((err) => console.log(err));
+    };
+
+    navtype = () => {
+        if (localStorage.getItem('token')) {
+            if (localStorage.getItem("role") === "user") {
+                return this.state.navLogin
+            } else {
+                return this.state.navAdmin
+            }
+        } else {
+            return this.state.navnotLogin
+        }
+    }
+
+    render() {
+        titlebar("Coffee Addict | Product")
+        return (
+            <>
+                <this.navtype />
+
+                <div className='container-fluid border-top'>
+                    <div className=''>
+                        <div className='row d-flex justify-content-center'>
+                            <div className='col-lg-4 col-md-12 col-sm-12 border-end d-flex flex-column align-items-center w-sm-50 px-5'>
+                                <div className={`${styles["product-left"]} d-flex flex-column`}>
+                                    <span className={`${styles["title-promo"]} text-center mt-4`}>Promo Today</span>
+                                    <span className={`${styles["desc-promo"]} text-center mt-3 mb-5 px-5`}>Coupons will be updated every weeks. Check them out!</span>
+                                </div>
+                                {/* Card promo start */}
+                                <CardPromo />
+                                <CardPromo />
+                                <CardPromo />
+                                <CardPromo />
+                                {/* card promo end */}
+
+                                <button className={`${styles["apply-coupon"]} mt-5 rounded-5 w-100`}>Apply Coupon</button>
+                                <div className={`${styles["noted"]} d-flex flex-column my-5 justify-content-start w-100`}>
+                                    <span className="py-3">Terms and Condition</span>
+                                    <p>1. You can only apply 1 coupon per day</p>
+                                    <p>2. It only for dine in</p>
+                                    <p>3. Buy 1 get 1 only for new user</p>
+                                    <p>4. Should make member card to apply coupon</p>
+                                </div>
+                                <div className={`d-flex flex-column justify-content-start w-100 ${styles["edit-link-promo"]}`}>
+                                    <Link to="/updatepromo">Edit Promo</Link>
+                                    <Link to="/updatepromo">Add New Promo</Link>
+                                </div>
+                            </div>
+
+                            <div className='col-lg-8 col-md-12 col-sm-12'>
+                                <div className={`${styles["nav-product"]} d-flex flex-row justify-content-around mt-4`}>
+                                    <span><p className={styles.point} onClick={this.Favorite} >Favorite & Promo</p></span>
+                                    <span><p className={styles.point} onClick={this.Coffee}>Coffee</p></span>
+                                    <span><p className={styles.point} onClick={this.NonCoffee}>Non Coffee</p></span>
+                                    <span><p className={styles.point} onClick={this.Food}>Foods</p></span>
+                                    <span><p className={styles.point} onClick={this.AddOn}>Add-on</p></span>
+                                </div>
+                                <div className={`row ${styles["list-content"]} d-flex justify-content-start`}>
+                                    {this.state.product.map((e, key) => (
+                                        <Cardproduct
+                                            key={`${key}`}
+                                            id={e.id}
+                                            params={e.id}
+                                            product_name={e.name}
+                                            price={this.costing(e.price)}
+                                            image_product={e.image} />
+                                    ))}
+
+                                </div>
+                                <div className="ms-5 mt-3">
+                                    <p className={styles["note-price"]}>*the price has been cutted by discount appears</p>
+                                </div>
+                                <div className={`d-flex flex-column justify-content-start w-100 ms-5 mt-4 mb-3 ${styles["edit-link-promo"]}`} >
+                                    <Link to="/updateproduct">Edit Product</Link>
+                                    <Link to="/updateproduct" className="mt-2">Add New Product</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Footer />
+            </>
+        )
+    }
+}
+
+export default withParams(Handlingproduct)
---
+++ src/pages/admin/UpdateProduct.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import component
+import Navbar from "../../components/Navbar"
+import Footer from "../../components/Footer"
+
+// import css
+import styles from "../../styles/adminCSS/Updateproduct.module.css"
+
+
+// import images
+import img_product from "../../asset/profil-bg.png";
+
+
+export default function UpdateProduct() {
+    return (
+        <>
+            <Navbar />
+            <div className='container-fluid border-top mb-5'>
+                {/* breadcrumb */}
+                <div className='container'>
+                    <div className='row py-3'>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className={`breadcrumb-item ${styles["step-one"]}`}><Link to="/product">Product</Link></li>
+                                <li className={`breadcrumb-item ${styles["step-two"]}`} >Add new product</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+                {/* breadcrumb */}
+
+                {/* Left Content */}
+                <div className='container'>
+                    <div className='row gap-2 d-flex justify-content-between'>
+                        <section className='col-lg-5 col-md-12 col-sm-12 d-flex flex-column align-items-center '>
+                            <div className='pt-5'>
+                                <img className='rounded-circle' src={img_product} alt='img_product' width="250px" height="250px"></img>
+                            </div>
+                            <button className={`${styles["change-profile"]} mt-5 rounded-5`}>Save Change</button>
+                            <div className={`${styles["profile-image"]} text-center rounded-5 mt-3 mb-5`}>
+                                <label for="img-profile">Choose Photo</label>
+                                <input type="file" name="" id="img-profile" />
+                            </div>
+                            <div className={`${styles[`delivery-hour`]}`}>
+                                <p >Delivery Hour :</p>
+                            </div>
+                            <div className='mt-3 w-100 d-flex align-item-center justify-content-center'>
+                                <select className={` ${styles["dropdown-hour"]} ps-3`} >
+                                    <option selected>Select start hour</option>
+                                    <option value="1">1 hour</option>
+                                    <option value="2">2 hour</option>
+                                    <option value="3">3 hour</option>
+                                    <option value="3">4 hour</option>
+                                    <option value="3">5  hour</option>
+                                </select>
+                            </div>
+                            <div className='mt-3 w-100 d-flex align-item-center justify-content-center'>
+                                <select className={` ${styles["dropdown-hour"]} mb-3 ps-3`} >
+                                    <option selected>Select end hour</option>
+                                    <option value="1">1 hour</option>
+                                    <option value="2">2 hour</option>
+                                    <option value="3">3 hour</option>
+                                    <option value="3">4 hour</option>
+                                    <option value="3">5  hour</option>
+                                </select>
+                            </div>
+                            <div className={`${styles[`delivery-hour`]} mt-5`}>
+                                <p >Input stock :</p>
+                            </div>
+                            <div className='mt-3 w-100 d-flex align-item-center justify-content-center mb-5'>
+                                <select className={`${styles["dropdown-hour"]} ps-3 `}>
+                                    <option selected>Input stock</option>
+                                    <option value="1">1 hour</option>
+                                    <option value="2">2 hour</option>
+                                    <option value="3">3 hour</option>
+                                    <option value="3">4 hour</option>
+                                    <option value="3">5  hour</option>
+                                </select>
+                            </div>
+                        </section>
+
+                        <section className='col-lg-6 col-md-12 col-sm-12 pt-5 '>
+                            <div className={`${styles["data-form"]} d-flex flex-column w-100`}>
+                                <label for="">Name :</label>
+                                <input type="email" name="" id="" placeholder="Type product name min. 50 characters" />
+                                <label for="">Price :</label>
+                                <input type="number" name="" id="" placeholder="Type the price" />
+                                <label for="">Description :</label>
+                                <input type="text" name="" id="" placeholder="Describe your product min. 150 characters" />
+                            </div>
+                            <div className={`${styles[`input-size`]} mt-5`}>
+                                <p className={styles['input-product-size']}>Input product size :</p>
+                                <p className={styles['input-product-desc']}>Click size you want to use for this product</p>
+                            </div>
+                            <div className={`${styles.size} d-flex justify-content-start text-center mt-3`}>
+                                <button className=" rounded-circle">R</button>
+                                <button className=" rounded-circle">L</button>
+                                <button className=" rounded-circle">XL</button>
+                            </div>
+                            <div className={`${styles[`input-size`]} mt-5`}>
+                                <p className={styles['input-product-size']}>Input delivery methods :</p>
+                                <p className={styles['input-product-desc']}>Click methods you want to use for this product</p>
+                            </div>
+                            <div className={`${styles.method} d-flex justify-content-start text-center mt-3`}>
+                                <button className=" rounded-3">Home delivery</button>
+                                <button className=" rounded-3">Dine in</button>
+                                <button className=" rounded-3">Take away</button>
+                            </div>
+                            <button className={`${styles["save-product"]} mt-5 rounded-5`}>Save Change</button>
+                            <button className={`${styles["cancel-product"]} mt-3 rounded-5`}>Cancel</button>
+                        </section>
+                    </div>
+                </div>
+                {/* Left Content */}
+
+
+
+
+            </div>
+            <Footer />
+        </>
+    )
+}
