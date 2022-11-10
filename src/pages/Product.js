536f29b060a99decdb606dfb2aa8d260136f6bf9
farisan <muhammad.farisan99@gmail.com>
2022-11-10 12:52:46 +0700

    revisi

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -51,9 +51,6 @@ class Product extends Component {
             .catch((err) => console.log(err));
     }
 
-    componentDidUpdate() {
-
-    }
 
     costing = (price) => {
         return 'IDR ' + parseFloat(price).toFixed().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
@@ -139,7 +136,7 @@ class Product extends Component {
                                     <span><p className={styles.point} onClick={() => {
                                         this.Favorite()
                                         this.setState({
-                                            searchParams: { sorting: "favorite" }
+                                            searchParams: { category: "favorite" }
                                         },
                                             () => {
                                                 this.props.setSearchParams(this.state.searchParams)
@@ -149,7 +146,7 @@ class Product extends Component {
                                     <span><p className={styles.point} onClick={() => {
                                         this.Coffee()
                                         this.setState({
-                                            searchParams: { sorting: "coffee" }
+                                            searchParams: { category: "coffee" }
                                         },
                                             () => {
                                                 this.props.setSearchParams(this.state.searchParams)
@@ -159,7 +156,7 @@ class Product extends Component {
                                     <span><p className={styles.point} onClick={() => {
                                         this.NonCoffee()
                                         this.setState({
-                                            searchParams: { sorting: "non coffee" }
+                                            searchParams: { category: "non coffee" }
                                         },
                                             () => {
                                                 this.props.setSearchParams(this.state.searchParams)
@@ -169,7 +166,7 @@ class Product extends Component {
                                     <span><p className={styles.point} onClick={() => {
                                         this.Food()
                                         this.setState({
-                                            searchParams: { sorting: "food" }
+                                            searchParams: { category: "food" }
                                         },
                                             () => {
                                                 this.props.setSearchParams(this.state.searchParams)
@@ -179,7 +176,7 @@ class Product extends Component {
                                     <span><p className={styles.point} onClick={() => {
                                         this.AddOn()
                                         this.setState({
-                                            searchParams: { sorting: "addon" }
+                                            searchParams: { category: "addon" }
                                         },
                                             () => {
                                                 this.props.setSearchParams(this.state.searchParams)
@@ -216,137 +213,5 @@ class Product extends Component {
 
 
 export default withSerchParams(withParams(Product));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function Product() {
-//     titlebar("Coffee Addict | Product")
-
-//     const [product, setProduct] = useState([])
-//     const [url, setUrl] = useState("http://localhost:6060/coffee/product")
-
-//     useEffect(() => {
-//         axios.get(url)
-//             .then(res => {
-//                 // console.log(res.data.result.data);
-//                 setProduct(res.data.result.data)
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//             })
-//     }, [url])
-
-//     // console.log(product);
-
-//     return (
-//         <>
-//             <Navbar />
-//             <section className={`${styles["style-bd"]} container-fluid d-flex flex-row flex-wrap`}>
-//                 <aside className={`${styles["product-left"]} d-flex flex-column align-items-center`}>
-//                     <span className={`${styles["title-promo"]} text-center mt-4`}>Promo Today</span>
-//                     <span className={`${styles["desc-promo"]} text-center mt-3 px-5`}>Coupons will be updated every weeks. Check them out!
-//                     </span>
-//                     <div className={`${styles["promo-one"]} d-flex mt-5 py-2 `}>
-//                         <Link to="">
-//                             <img src={promo_one} alt="pic-promo" />
-//                         </Link>
-//                         <div className={`${styles["description-promo"]} py-2`}>
-//                             <span>HAPPY MOTHER'S DAY!</span>
-//                             <p>Get one of our favorite menu for free!</p>
-//                         </div>
-//                     </div>
-//                     <div className={`${styles["promo-two"]} d-flex mt-3 mx-3 py-2 bg-warning`}>
-//                         <Link to="">
-//                             <img src={promo_two} alt="pic-promo" />
-//                         </Link>
-//                         <div className={`${styles["description-promo"]} py-2`}>
-//                             <span>Get a cup of coffee for free on sunday morning</span>
-//                             <p>Only at 7 to 9 AM</p>
-//                         </div>
-//                     </div>
-//                     <div className={`${styles["promo-one"]} d-flex mt-3 py-2 `}>
-//                         <Link to="">
-//                             <img src={promo_one} alt="pic-promo" />
-//                         </Link>
-//                         <div className={`${styles["description-promo"]} py-2`}>
-//                             <span>HAPPY MOTHER'S DAY!</span>
-//                             <p>Get one of our favorite menu for free!</p>
-//                         </div>
-//                     </div>
-//                     <div className={`${styles["promo-four"]} d-flex mt-3 mx-3 `}>
-//                         <Link to="">
-//                             <img src={promo_four} alt="pic-promo" />
-//                         </Link>
-//                         <div className={`${styles["description-promo"]} py-3`}>
-//                             <span>HAPPY HALLOWEEN!</span>
-//                             <p>Do you like chicken wings? Get 1 free only if you buy pinky promise</p>
-//                         </div>
-//                     </div>
-//                     <button className={`${styles["apply-coupon"]} mt-5 rounded-5`}>Apply Coupon</button>
-//                     <div className={`${styles["noted"]} d-flex flex-column my-5`}>
-//                         <span className="py-3">Terms and Condition</span>
-//                         <p>1. You can only apply 1 coupon per day</p>
-//                         <p>2. It only for dine in</p>
-//                         <p>3. Buy 1 get 1 only for new user</p>
-//                         <p>4. Should make member card to apply coupon</p>
-//                     </div>
-//                 </aside>
-
-//                 <aside className={`${styles["product-right"]} d-flex flex-column py-4`}>
-//                     <div className={`${styles["nav-product"]} d-flex flex-row justify-content-around`}>
-//                         <span><Link to="">Favorite & Promo</Link></span>
-//                         <span><Link to="">Coffee</Link></span>
-//                         <span><Link to="">Non Coffee</Link></span>
-//                         <span><Link to="">Foods</Link></span>
-//                         <span><Link to="">Add-on</Link></span>
-//                     </div>
-
-//                     {/* Card Product */}
-//                     <section className="container-fluid text-center ps-5 ms-4">
-//                         <div className={`row ${styles["list-content"]} justify-content-start ${styles["gap-Row"]} ${styles["position-settings"]}`}>
-//                             {product.map((e) => (
-//                                 <CardProduct product_name={e.name} price={e.price} image_product={e.image} />
-//                             ))}
-//                         </div>
-//                     </section>
-//                 </aside>
-//             </section>
-//             <Footer />
-//         </>
-//     )
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
