1c759804bbfdb48ef682ee2bc6de2fed238123c0
farisan <muhammad.farisan99@gmail.com>
2023-01-06 00:00:47 +0700

    change rute otp

--- src/pages/auth/Otp.js
+++ src/pages/auth/Otp.js
@@ -8,12 +8,14 @@ import css from "../../styles/Auth.module.css";
 function Otp() {
 
   const { otp } = useParams();
+  console.log(otp)
   const navigate = useNavigate()
 
   useEffect(() => {
+    console.log(otp)
     axios
     .get(
-      `${process.env.REACT_APP_BACKEND_HOST}/auth/${otp}`,
+      `${process.env.REACT_APP_BACKEND_HOST}/users/verify/${otp}`,
     )
     .then(() => console.log("success"))
     .catch((err) => console.log(err));
